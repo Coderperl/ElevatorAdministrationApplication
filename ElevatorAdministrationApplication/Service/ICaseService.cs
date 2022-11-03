7d160cdff9e74f8b220b9ce28589952a968c1989
Per görtz <pelle_gortz@hotmail.com>
2022-11-03 15:06:09 +0100

    Created simple list page for cases, added links for update, create, and details page.

---
+++ ElevatorAdministrationApplication/Service/ICaseService.cs
@@ -0,0 +1,12 @@
+﻿using ElevatorAdministrationApplication.Models;
+
+namespace ElevatorAdministrationApplication.Service
+{
+    public interface ICaseService
+    {
+        public CaseModel GetCase(int id);
+        public List<CaseModel> GetCases();
+        public Status UpdateCases(CaseModel updateCase, int Id);
+        public Status CreateCase(CaseModel createCase);
+    }
+}
