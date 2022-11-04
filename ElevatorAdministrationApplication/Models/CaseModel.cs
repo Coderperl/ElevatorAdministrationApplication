61d9b1b8a05bca897e5f799d65dbb76db1564dca
AmirHusseini <56257612+AmirHusseini@users.noreply.github.com>
2022-11-04 11:33:58 +0100

    testing statistics ...

---
+++ ElevatorAdministrationApplication/Models/CaseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ElevatorAdministrationApplication.Models
+{
+    public class CaseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ElevatorModel Elevator { get; set; }
+        public TechnicianModel Technician { get; set; }
+        public string Status { get; set; }
+    }
+}
--- ElevatorAdministrationApplication/Pages/Statistics/StatisticsPage.cshtml.cs
+++ ElevatorAdministrationApplication/Pages/Statistics/StatisticsPage.cshtml.cs
@@ -1,12 +1,56 @@
+using ElevatorAdministrationApplication.Models;
+using Google.DataTable.Net.Wrapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NuGet.Protocol;
+using System;
+using System.Text.Json;
 
 namespace ElevatorAdministrationApplication.Pages.Statistics
 {
     public class StatisticsPageModel : PageModel
     {
-        public void OnGet()
+        public void OnGetAsync()
         {
+            
         }
+
+        private async Task<ElevatorModel[]> AllElevatorsAsync()
+        {
+            HttpClient client = new HttpClient();
+            var stream = client.GetStreamAsync("https://agilewebapi.azurewebsites.net/api/Elevator");
+            var data = await JsonSerializer.DeserializeAsync<ElevatorModel[]>(await stream);
+            
+            return data;
+        }
+        public async Task<ActionResult> OnGetElevatorsAsync()
+        {
+            var elevators = await AllElevatorsAsync();
+            int active = 0;
+            int inactive = 0;
+            var data = elevators.Select(a => new
+            {
+                Name = a.ElevatorStatus.Equals("Active") ? ++active : ++inactive,
+                Count = a.ElevatorStatus.Count(),
+
+            }).ToList();
+
+            var dt = new Google.DataTable.Net.Wrapper.DataTable();
+            dt.AddColumn(new Column(ColumnType.String, "Status", "Status"));
+            dt.AddColumn(new Column(ColumnType.Number, "Active", "Active"));
+            dt.AddColumn(new Column(ColumnType.Number, "InActive", "InActive"));
+
+            var row1 = dt.NewRow();
+            row1.AddCell(new Cell("Status"));
+            row1.AddCell(new Cell(active));
+            row1.AddCell(new Cell(inactive));
+            dt.AddRow(row1);
+            return Content(dt.GetJson());
+        }
+        //var row2 = dt.NewRow();
+        //row2.AddCell(new Cell("InActive"));
+        ////row2.AddCell(new Cell(active));
+        //row2.AddCell(new Cell(inactive));
+        //dt.AddRow(row2);
     }
 }
