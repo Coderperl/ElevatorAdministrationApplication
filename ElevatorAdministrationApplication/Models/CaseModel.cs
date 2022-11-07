61cd56c8b4731867f608b49c4bb879044d318d22
AmirHusseini <56257612+AmirHusseini@users.noreply.github.com>
2022-11-07 11:30:32 +0100

    added Piechart for cases.

--- ElevatorAdministrationApplication/Models/CaseModel.cs
+++ ElevatorAdministrationApplication/Models/CaseModel.cs
@@ -1,11 +1,17 @@
-﻿namespace ElevatorAdministrationApplication.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ElevatorAdministrationApplication.Models
 {
     public class CaseModel
     {
-        public int Id { get; set; }
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+        [JsonPropertyName("elevator")]
         public ElevatorModel Elevator { get; set; }
+        [JsonPropertyName("technician")]
         public TechnicianModel Technician { get; set; }
+        [JsonPropertyName("status")]
         public string Status { get; set; }
-    }
+        
+}
 }
--- ElevatorAdministrationApplication/Pages/Statistics/StatisticsPage.cshtml.cs
+++ ElevatorAdministrationApplication/Pages/Statistics/StatisticsPage.cshtml.cs
@@ -10,31 +10,39 @@ namespace ElevatorAdministrationApplication.Pages.Statistics
 {
     public class StatisticsPageModel : PageModel
     {
+        
         public void OnGetAsync()
         {
             
         }
-
         private async Task<ElevatorModel[]> AllElevatorsAsync()
         {
             HttpClient client = new HttpClient();
             var stream = client.GetStreamAsync("https://agilewebapi.azurewebsites.net/api/Elevator");
             var data = await JsonSerializer.DeserializeAsync<ElevatorModel[]>(await stream);
-            
+
             return data;
         }
-        public async Task<ActionResult> OnGetElevatorsAsync()
+        private async Task<CaseModel[]> AllCasesAsync()
+        {
+            HttpClient client = new HttpClient();
+            var stream = client.GetStreamAsync("https://agilewebapi.azurewebsites.net/api/Case");
+            var data = await JsonSerializer.DeserializeAsync<CaseModel[]>(await stream);
+
+            return data;
+        }
+
+        public async Task<IActionResult> OnGetElevatorsStatus()
         {
             var elevators = await AllElevatorsAsync();
             int active = 0;
             int inactive = 0;
+
             var data = elevators.Select(a => new
             {
-                Name = a.ElevatorStatus.Equals("Active") ? ++active : ++inactive,
-                Count = a.ElevatorStatus.Count(),
+                Name = a.ElevatorStatus.Equals("Active") ? ++active : ++inactive
 
             }).ToList();
-
             var dt = new Google.DataTable.Net.Wrapper.DataTable();
             dt.AddColumn(new Column(ColumnType.String, "Status", "Status"));
             dt.AddColumn(new Column(ColumnType.Number, "Active", "Active"));
@@ -45,12 +53,35 @@ namespace ElevatorAdministrationApplication.Pages.Statistics
             row1.AddCell(new Cell(active));
             row1.AddCell(new Cell(inactive));
             dt.AddRow(row1);
+            
             return Content(dt.GetJson());
         }
-        //var row2 = dt.NewRow();
-        //row2.AddCell(new Cell("InActive"));
-        ////row2.AddCell(new Cell(active));
-        //row2.AddCell(new Cell(inactive));
-        //dt.AddRow(row2);
+        public async Task<IActionResult> OnGetCasesStatus()
+        {
+            var cases = await AllCasesAsync();
+            int started = 0;
+            int notstarted = 0;
+
+            var data = cases.Select(a => new
+            {
+                Status = a.Status.Equals("Started") ? ++started :++notstarted
+
+            }).ToList();
+            var dt = new Google.DataTable.Net.Wrapper.DataTable();
+            dt.AddColumn(new Column(ColumnType.String, "Name", "Name"));
+            dt.AddColumn(new Column(ColumnType.Number, "Status", "Status"));
+
+            var row1 = dt.NewRow();
+            row1.AddCell(new Cell("Started"));
+            row1.AddCell(new Cell(started));
+            dt.AddRow(row1);
+
+            var row2 = dt.NewRow();
+            row2.AddCell(new Cell("Not Started"));
+            row2.AddCell(new Cell(notstarted));
+            dt.AddRow(row2);
+
+            return Content(dt.GetJson());
+        }
     }
 }
