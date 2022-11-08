93ade9cd8860ce4c790e0306a64151d451aae24c
AmirHusseini <56257612+AmirHusseini@users.noreply.github.com>
2022-11-08 10:39:19 +0100

    Revert "deleted casemodel.cs!"
    This reverts commit 9ebb982f472b407a008523e61e6ed5cce255ca98.

---
+++ ElevatorAdministrationApplication/Models/CaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ElevatorAdministrationApplication.Models
+{
+    public class CaseModel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("elevator")]
+        public ElevatorModel Elevator { get; set; }
+        [JsonPropertyName("technician")]
+        public TechnicianModel Technician { get; set; }
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+        
+}
+}
