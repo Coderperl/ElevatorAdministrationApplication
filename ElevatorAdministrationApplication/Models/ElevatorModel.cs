e6c3dd9ef6cab92243baa5e83a03925e6c521470
AmirHusseini <56257612+AmirHusseini@users.noreply.github.com>
2022-11-08 10:39:07 +0100

    Revert "as"
    This reverts commit 88920f96e1647ac343b823533f50498d78ce04f3.

---
+++ ElevatorAdministrationApplication/Models/ElevatorModel.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace ElevatorAdministrationApplication.Models
+{
+    public class ElevatorModel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("address")]
+        public string Address { get; set; }
+        [JsonPropertyName("lastInspection")]
+        public DateTime LastInspection { get; set; }
+        [JsonPropertyName("nextInspection")]
+        public DateTime NextInspection { get; set; }
+        [JsonPropertyName("maximumWeight")]
+        public string MaximumWeight { get; set; }
+        [JsonPropertyName("reboot")]
+        public bool Reboot { get; set; }
+        [JsonPropertyName("shutDown")]
+        public bool ShutDown { get; set; }
+        [JsonPropertyName("door")]
+        public bool Door { get; set; }
+        [JsonPropertyName("floor")]
+        public int Floor { get; set; }
+        [JsonPropertyName("elevatorStatus")]
+        public string ElevatorStatus { get; set; }
+    }
+}
