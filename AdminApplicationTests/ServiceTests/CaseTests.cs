1b0ce63503f4ea9a5446c466b02bdfce5c0e4569
Per görtz <pelle_gortz@hotmail.com>
2022-11-22 11:22:04 +0100

    cases service additions

---
+++ AdminApplicationTests/ServiceTests/CaseTests.cs
@@ -0,0 +1,42 @@
+﻿using ElevatorAdministrationApplication.Models;
+using ElevatorAdministrationApplication.Service;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorAdministrationApplicationTests.ServiceTests
+{
+    public class CaseTests
+    {
+        
+        private Mock<ICaseService> _sut;
+        public CaseTests()
+        {
+            _sut = new Mock<ICaseService>();
+        }
+
+        [Fact]
+        public void GetCases_returns_a_list_of_CaseModel()
+        {
+            //Arrange
+            var caseList = new List<CaseModel>();
+            //Act
+            _sut.Setup(a => a.GetCases()).Returns(caseList);
+            //Assert
+            Assert.IsType<List<CaseModel>>(_sut.Object.GetCases());
+        }
+        [Fact]
+        public void GetCase_returns_CaseModel()
+        {
+            //Arrange
+            var Case = new CaseModel();
+            //Act
+            _sut.Setup(a => a.GetCase(It.IsAny<int>())).Returns(Case);
+            //Assert
+            Assert.IsType<CaseModel>(_sut.Object.GetCase(It.IsAny<int>()));
+        }
+    }
+}
