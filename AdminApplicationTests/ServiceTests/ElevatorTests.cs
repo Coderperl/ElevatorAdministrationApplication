73651ea6a5ea0f985ac3b01a3cfa2ba09d8952cd
Christoffer K <henricson21@hotmail.com>
2022-11-22 09:22:13 +0100

    elevator tests

---
+++ AdminApplicationTests/ServiceTests/ElevatorTests.cs
@@ -0,0 +1,37 @@
+﻿using ElevatorAdministrationApplication.Models;
+using ElevatorAdministrationApplication.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace ElevatorAdministrationApplicationTests.ServiceTests
+{
+    public class ElevatorTests
+    {
+        private readonly ElevatorService _sut;
+        
+        public ElevatorTests()
+        {
+            _sut = new ElevatorService();
+        }
+
+        [Fact]
+        public void GetElevators_returns_a_list_of_elevatormodel()
+        {
+            var elevators = _sut.GetElevators();
+
+            Assert.IsType<List<ElevatorModel>>(elevators);
+        }
+        [Fact]
+        public void GetElevator_returns_elevatormodel()
+        {
+            var elevator = _sut.GetElevator(1);
+
+            Assert.IsType<ElevatorModel>(elevator);
+        }
+    }
+}
---
+++ ElevatorAdministrationApplication/API/WebApi.cs
@@ -0,0 +1,33 @@
+﻿namespace ElevatorAdministrationApplication.API
+{
+    public class WebApi : IWebApi
+    {
+        public async Task<ReturnData> apiReturnAsync(string api)
+        {
+            ReturnData returnData = new ReturnData();
+
+            using var httpClient = new HttpClient();
+
+            var data = new HttpResponseMessage();
+
+            try
+            {
+                data = await httpClient.GetAsync($"{api}");
+            }
+            catch
+            {
+                return returnData;
+            }
+            
+            if(data.IsSuccessStatusCode)
+            {
+                returnData.Data = await data.Content.ReadAsStringAsync();
+                returnData.Status = data.StatusCode;
+            }
+
+            return returnData;
+        }
+    }
+
+
+}
